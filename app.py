148114a0d1dcf5b07c7941d881340fc4e3264b3a
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-14 08:35:19 +0300

    Complete setup for app, database configuration and migrations

---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from flask import Flask, make_response, jsonify
+from flask_migrate import Migrate
+
+from models import db, Hero, HeroPower, Power
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+
+@app.route('/')
+def home():
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
